b0e77975511b913f8716c9951c18d686f3b6fbea
David Elì <davidelimg1@gmail.com>
2020-05-04 19:28:09 -0500

    Building logic of the program

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -1,4 +1,15 @@
 const Dom = (() => {
+  const name = document.getElementById('task-name').value;
+  const description = document.getElementById('task-description');
+
+  const verifyFields = () => {
+    
+  };
+
+  return {
+    name,
+    description
+  }
 
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dom from './dom-utils.js';
+
+const domItem = Dom;
+
+const name = domItem.name;
+console.log(name);
